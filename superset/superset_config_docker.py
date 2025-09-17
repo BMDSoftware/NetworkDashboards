18d9aa0214663aba9e2ec687a1f56d7c548bdf66
José Frias <josefrias@bmd-software.com>
2025-09-17 17:19:55 +0100

    Add several configs for superset in .env

--- superset/superset_config_docker.py
+++ superset/superset_config_docker.py
@@ -1,5 +1,6 @@
 from datetime import timedelta
 import os
+import ast
 
 FEATURE_FLAGS = {
     "DASHBOARD_NATIVE_FILTERS": False,
@@ -7,8 +8,34 @@ FEATURE_FLAGS = {
     "GENERIC_CHART_AXES": True,
     "ENABLE_TEMPLATE_PROCESSING": True,
     "ENABLE_TEMPLATE_REMOVE_FILTERS": True,
-    "FLATTEN_INDEX_ON_CSV_EXPORT": True
+    "FLATTEN_INDEX_ON_CSV_EXPORT": True,
+    "EMBEDDED_SUPERSET": os.environ.get("FEATURE_EMBEDDED_SUPERSET", "True") == "True"
 }
+
+# Guest token settings
+GUEST_ROLE_NAME = os.environ.get("GUEST_ROLE_NAME")
+GUEST_TOKEN_JWT_SECRET = os.environ.get("GUEST_TOKEN_JWT_SECRET")
+GUEST_TOKEN_JWT_AUDIENCE = os.environ.get("GUEST_TOKEN_JWT_AUDIENCE")
+GUEST_TOKEN_JWT_EXP_SECONDS = int(os.environ.get("GUEST_TOKEN_JWT_EXP_SECONDS"))
+
+# Talisman / CORS
+TALISMAN_ENABLED = os.environ.get("TALISMAN_ENABLED", "True") == "True"
+ENABLE_CORS = os.environ.get("ENABLE_CORS", "True") == "True"
+CORS_OPTIONS = {
+    "origins": ast.literal_eval(f"[{os.environ.get('CORS_ORIGINS', '')}]"),
+    "supports_credentials": True
+}
+
+TALISMAN_CONFIG = {
+    "content_security_policy": {
+        "frame-ancestors": ast.literal_eval(os.environ.get("CSP_FRAME_ANCESTORS", "['self']"))
+    },
+    "force_https": os.environ.get("TALISMAN_FORCE_HTTPS", "False") == "True",
+    "session_cookie_secure": os.environ.get("TALISMAN_SESSION_COOKIE_SECURE", "True") == "True",
+}
+
+# CSRF headers
+WTF_CSRF_HEADERS = [os.environ.get("WTF_CSRF_HEADERS", "X-CSRFToken")]
 
 CHARTS_FOR_PIVOTED_CSV_EXPORT = []
 if os.environ.get("CHARTS_FOR_PIVOTED_CSV_EXPORT", None) is not None:
